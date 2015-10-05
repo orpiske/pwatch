b82363adc8d33f28484342527c894ecb76a53326
Otavio Rodolfo Piske <opiske@redhat.com>
2015-10-05 16:23:00 +0200

    Fixed formatting

--- src/atexit/main.c
+++ src/atexit/main.c
@@ -16,115 +16,121 @@
 
 #include "main.h"
 
-static void messenger_function(message_level_t level, const char *msg, ...) {
-    va_list ap;
-    char *ret = NULL;
+static void messenger_function(message_level_t level, const char *msg, ...)
+{
+	va_list ap;
+	char *ret = NULL;
 
-    va_start(ap, msg);
-    vasprintf(&ret, msg, ap);
+	va_start(ap, msg);
+	vasprintf(&ret, msg, ap);
 
-    switch (level) {
-        case TRACE:
-        {
-            fprintf(stderr, "[TRACE]: %s\n", ret);
-            break;
-        }
-        case DEBUG:
-        {
-            const options_t *options = get_options_object();
+	switch (level) {
+	case TRACE:
+	{
+		fprintf(stderr, "[TRACE]: %s\n", ret);
+		break;
+	}
+	case DEBUG:
+	{
+		const options_t *options = get_options_object();
 
-            if (options && options->debug) {
-                fprintf(stderr, "[DEBUG]: %s\n", ret);
-            }
-            break;
-        }
-        case INFO:
-        {
-            fprintf(stderr, "[INFO]: %s\n", ret);
-            break;
-        }
-        case WARNING:
-        {
-            fprintf(stderr, "[WARNING]: %s\n", ret);
-            break;
-        }
-        case ERROR:
-        {
-            fprintf(stderr, "[ERROR]: %s\n", ret);
-            break;
-        }
-        case FATAL:
-        {
-            fprintf(stderr, "[FATAL]: %s\n", ret);
-            break;
-        }
-        default:
-        {
-            fprintf(stderr, "[MSG]: %s\n", ret);
-            break;
-        }
-    }
+		if (options && options->debug) {
+			fprintf(stderr, "[DEBUG]: %s\n", ret);
+		}
+		break;
+	}
+	case INFO:
+	{
+		fprintf(stderr, "[INFO]: %s\n", ret);
+		break;
+	}
+	case WARNING:
+	{
+		fprintf(stderr, "[WARNING]: %s\n", ret);
+		break;
+	}
+	case ERROR:
+	{
+		fprintf(stderr, "[ERROR]: %s\n", ret);
+		break;
+	}
+	case FATAL:
+	{
+		fprintf(stderr, "[FATAL]: %s\n", ret);
+		break;
+	}
+	default:
+	{
+		fprintf(stderr, "[MSG]: %s\n", ret);
+		break;
+	}
+	}
 
-    va_end(ap);
-    free(ret);
+	va_end(ap);
+	free(ret);
 }
 
-int main(int argc, char **argv) {
-    int c;
-    int option_index = 0;
+int main(int argc, char **argv)
+{
+	int c;
+	int option_index = 0;
 
-    options_t *options = options_new();
+	options_t *options = options_new();
 
-    if (!options) {
-        return EXIT_FAILURE;
-    }
+	if (!options) {
+		return EXIT_FAILURE;
+	}
 
-    pid_t pid = 0;
+	pid_t pid = 0;
 
-    set_options_object(options);
-    set_messenger(messenger_function);
-    while (1) {
+	set_options_object(options);
+	set_messenger(messenger_function);
+	while (1) {
 
-        static struct option long_options[] = {
-            { "pid", true, 0, 'p'},
-            { "debug", false, 0, 'd'},
-	    { "trace", false, 0, 't'},
-            { "command", true, 0, 'c'},
-            { "help", false, 0, 'h'},
-            { 0, 0, 0, 0}
-        };
+		static struct option long_options[] = {
+			{ "pid", true, 0, 'p'},
+			{ "debug", false, 0, 'd'},
+			{ "trace", false, 0, 't'},
+			{ "command", true, 0, 'c'},
+			{ "help", false, 0, 'h'},
+			{ 0, 0, 0, 0}
+		};
 
-        c = getopt_long(argc, argv, "p:dc:H", long_options, &option_index);
-        if (c == -1) {
-            if (optind == 1) {
-                fprintf(stderr, "Not enough options\n");
-                return EXIT_FAILURE;
-            }
-            break;
-        }
+		c = getopt_long(argc, argv, "p:dc:H", long_options, &option_index);
+		if (c == -1) {
+			if (optind == 1) {
+				fprintf(stderr, "Not enough options\n");
+				return EXIT_FAILURE;
+			}
+			break;
+		}
 
-        switch (c) {
-            case 'p': {
-                options->pid = strtol(optarg, NULL, 10);
-                break;
-            }
-            case 'd': {
-                options->debug= true;
-                break;
-            }
-            case 'c': {
-                strncpy(options->command, optarg, sizeof(options->command) - 1);
-                break;
-            }
-	    case 't': {
-                options->trace = true;
-                break;
-            }
-        }
-    }
+		switch (c) {
+		case 'p':
+		{
+			options->pid = strtol(optarg, NULL, 10);
+			break;
+		}
+		case 'd':
+		{
+			options->debug = true;
+			break;
+		}
+		case 'c':
+		{
+			strncpy(options->command, optarg, sizeof(options->command) - 1);
+			break;
+		}
+		case 't':
+		{
+			options->trace = true;
+			break;
+		}
+		}
+	}
 
 
-    trace_start();
+	trace_start();
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
